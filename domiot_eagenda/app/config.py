8b9e12c737a4b6e152d07b25d73f5ea269f53e92
thdemarty <48022911+thdemarty@users.noreply.github.com>
2025-04-24 17:01:29 +0200

    feat(back) add missconfiguration and noaccess template

--- domiot_eagenda/app/config.py
+++ domiot_eagenda/app/config.py
@@ -5,3 +5,4 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    CORE_ADDON_HOSTNAME = os.environ.get("FLASK_CORE_ADDON_HOSTNAME")
