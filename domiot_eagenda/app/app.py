de6551a46151a36cec8980e3a72d06a3c86de927
thdemarty <48022911+thdemarty@users.noreply.github.com>
2025-04-24 13:55:52 +0200

    feat(path) overwrite url_for in template context processor
    Co-authored-by: Infinitium <tristan.henault@orange.fr>

--- domiot_eagenda/app/app.py
+++ domiot_eagenda/app/app.py
@@ -2,11 +2,21 @@ from flask import Flask
 from config import Config
 from flask_migrate import Migrate
 from models import db
+from utils import url_for
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db.init_app(app)
 migrate = Migrate(app, db)
+
+
+@app.context_processor
+def inject_url_for():
+    """
+    Inject the `url_for` monkeypatch into the template context. 
+    use the `url_for` function in the templates.
+    """
+    return dict(url_for=url_for)
 
 @app.route('/')
 def index():
